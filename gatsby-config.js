af3ac0c54803d959e9163a7c89597449b16c3773
Marie Hogebrandt <marie.hogebrandt@angrycreative.se>
2019-02-09 12:30:10 +0100

    Fix description and name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Marie Hogebrandt`,
+    author: `Marie Hogebrandt`,
+    description: `Professional site for Marie Hogebrandt.`,
+    siteUrl: `https://mariehogebrandt.se`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `melindrea`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -46,7 +46,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile-400x400.jpeg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
